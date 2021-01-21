94d04075b03bfe5d7d2620d497b8597977e79655
Paul-Munley <paulrmunley21@gmail.com>
2021-01-21 15:51:46 -0700

    Worked on html and css aligning to fit page better.

--- CoinFlipDapp/contracts/CoinFlip.sol
+++ CoinFlipDapp/contracts/CoinFlip.sol
@@ -27,12 +27,12 @@ contract CoinFlip {
 	}
 
 	//Add funds from contractBalance to betAmount variable (aka set betAmount)
-    // function setBet(uint amount) public payable {
-    //     // require(amount <= contractBalance, "Insufficient funds. Please bet equal to or less than your current balance amount. Or add more to your balance.");
-    //     //require(minimumBet <= amount, "Minimum bet should be at least 1 ether"); **CHECK NOTE BELOW**
-	// 	contractBalance -= amount;
-	// 	betAmount = amount;
-    // }
+    function setBet(uint amount) public payable {
+        // require(amount <= contractBalance, "Insufficient funds. Please bet equal to or less than your current balance amount. Or add more to your balance.");
+        //require(minimumBet <= amount, "Minimum bet should be at least 1 ether"); **CHECK NOTE BELOW**
+		contractBalance -= amount;
+		betAmount = amount;
+    }
 
 	//Function to choose heads/tails choice, run psudo-random coin flip, and run result if choser won/lost
 	function random(uint256 choice) public payable returns(bool){
@@ -43,9 +43,11 @@ contract CoinFlip {
 		if(result == choice){
 		    contractBalance += betAmount * 2;
 		    success = true;
+			betAmount = 0;
 		}
 		else{
 		    success = false;
+			betAmount = 0;
 		}
 		return success;
 	}
