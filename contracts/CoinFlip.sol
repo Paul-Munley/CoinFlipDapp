f18c097e6ffc45aab1d850b09a1a46587d565160
Paul-Munley <paulrmunley21@gmail.com>
2021-01-27 15:27:27 -0700

    Connected contract to ropsten network for first time and started working on editing contract to connect and work with provable oracle

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -1,14 +1,38 @@
+import "./provableAPI_0.6.sol";
+
 pragma solidity 0.7.6;
 
-contract CoinFlip {
+contract CoinFlip is usingProvable {
 
     // mapping (address => uint) balance;
+	uint256 constant NUM_RANDOM_BYTES_REQUESTED = 1;
+	uint256 public latestNumber;
 
 	uint256 public contractBalance;
     uint256 public betAmount;
 
-	constructor() {
+	constructor() public {
 		contractBalance = 0;
+		update(); //for random number oracle
+	}
+
+	function __callback(bytes32 _queryId, string memory _result, bytes memory _proof) public {
+		require(msg.sender == provable_cbAddress());
+
+		uint256 randomNumber = uint256(keccak256(abi.encodePacked(_result))) % 100;
+		latestNumber = randomNumber;
+		emit generatedRandomNumber(randomNumber);
+	}
+
+	function update() payable public {
+		uint256 QUERY_EXECUTION_DELAY = 0;
+		uint256 GAS_FOR_CALLBACK = 200000;
+		provable_newRandomDSQuery(
+			QUERY_EXECUTION_DELAY,
+			NUM_RANDOM_BYTES_REQUESTED,
+			GAS_FOR_CALLBACK
+		);
+		emit LogNewProvableQuery("Provable query was sent, standing by for the answer...");
 	}
 
 	//Add funds to contract balance (aka set contractBalance)
@@ -30,22 +54,28 @@ contract CoinFlip {
 		betAmount = amount;
     }
 
-	//Function to choose heads/tails choice, run psudo-random coin flip, and run result if choser won/lost
-	function random(uint256 choice) public payable returns(bool){
-	    require(choice == 0 || choice == 1, "invalid choice");
-	    //require(betAmount > 0);
-		bool success;
-		uint256 result = block.timestamp % 2;
-		if(result == choice){
-		    contractBalance += betAmount * 2;
-		    success = true;
-			betAmount = 0;
-		}
-		else{
-		    success = false;
-			betAmount = 0;
-		}
-		return success;
-	}
+	//Pt.2 random() real function
+	
+
+	//Pt.2 random() fake function to speed up production process instead of waiting for response from ropsten network
+
+
+	//Pt. 1 random() function. to choose heads/tails choice, run psudo-random coin flip, and run result if choser won/lost.
+	// function random(uint256 choice) public payable returns(bool){
+	//     require(choice == 0 || choice == 1, "invalid choice");
+	//     //require(betAmount > 0);
+	// 	bool success;
+	// 	uint256 result = block.timestamp % 2;
+	// 	if(result == choice){
+	// 	    contractBalance += betAmount * 2;
+	// 	    success = true;
+	// 		betAmount = 0;
+	// 	}
+	// 	else{
+	// 	    success = false;
+	// 		betAmount = 0;
+	// 	}
+	// 	return success;
+	// }
 
 }
--- truffle-config.js
+++ truffle-config.js
@@ -60,10 +60,10 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-    provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/61ebac4a03634e2f88ab0f33f6b9637e`),
+    provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/61ebac4a03634e2f88ab0f33f6b9637e`, 0),
     network_id: 3,       // Ropsten's id
-    gas: 4712388,        // Ropsten has a lower block limit than mainnet
-    confirmations: 1,    // # of confs to wait between deployments. (default: 0)
+    gas: 4700000,        // Ropsten has a lower block limit than mainnet
+    confirmations: 2,    // # of confs to wait between deployments. (default: 0)
     timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
     skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     }
